6d3dbd0ffe2198b96e05ec9551d6b75ebdf397d4
sheraz2045 <sheraz@iotsands.com>
2025-03-28 10:41:34 +0500

    inital requirements gathered

---
+++ lib/utils/assets.dart
@@ -0,0 +1,26 @@
+abstract class Assets {
+  //images
+  static const String product1 =
+      'assets/images/668adc4fcd57736f5c718ef0_product-06.png';
+  static const String product2 =
+      'assets/images/lg_PFcFK51Hy0J0x8dk2hJTpjSQoTA6wsxcdfAwtFMS.png';
+  static const String product3 =
+      'assets/images/coco-noir-chanel-pink-aesthetic-plrz6za4j9w81o44.png';
+  static const String product4 =
+      'assets/images/f6a5603cd8028babc9c5a74cc3327c99.png';
+  static const String product5 = 'assets/images/Glowify-gallery-img-1.png';
+  static const String prodyct6 =
+      'assets/images/668adc4fcd57736f5c718ef6_product-12.png';
+  static const String product7 =
+      'assets/images/l668adc4fcd57736f5c718ef9_product-18.png';
+  static const String profilePicture =
+      'assets/images/8ebb1801-6af2-4a3b-b0c4-ae4a7de2b09d_removalai_preview.png';
+
+  //icons
+  static const String arrowForwardIcon =
+      'assets/icons/arrows-direction-down-2-svgrepo-com.svg';
+  static const String searchIcon =
+      'assets/icons/search-normal-1-svgrepo-com.svg';
+  static const String shoppingBagIcon =
+      'assets/icons/shopping-bag-3-svgrepo-com.svg';
+}
